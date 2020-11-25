ce0b2f60e3812b6de052425908746afe8e91cdfd
Amitesh-Krishna <70693281+Amitesh-Krishna@users.noreply.github.com>
2020-11-25 13:16:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,18 +10,18 @@ function setup(){
 	world = engine.world;
 	//the pendulums
 	//the leftmost
-	pendulum1 = new pendulum((width-200)/5,height-100);
-	pendulum2 = new pendulum((width-200)/5 *2,height-100);
-	pendulum3 = new pendulum((width-200)/5 *3,height-100);
-	pendulum4 = new pendulum((width-200)/5 *4,height-100);
+	pendulum1 = new pendulum(401,height-100);
+	pendulum2 = new pendulum(428.5,height-100);
+	pendulum3 = new pendulum(456,height-100);
+	pendulum4 = new pendulum(483.5,height-100);
 	//the rightmost
-	pendulum5 = new pendulum((width-200)/5 *5,height-100);
+	pendulum5 = new pendulum(511,height-100);
 	// the slings
-	sling1 = new sling(pendulum1.body,{x:(width-200)/5, y:100});
-	sling2 = new sling(pendulum2.body,{x:(width-200)/5 *2, y:100});
-	sling3 = new sling(pendulum3.body,{x:(width-200)/5 *3, y:100});
-	sling4 = new sling(pendulum4.body,{x:(width-200)/5 *4, y:100});
-	sling5 = new sling(pendulum5.body,{x:(width-200)/5 *5, y:100});
+	sling1 = new sling(pendulum1.body,{x:401, y:100});
+	sling2 = new sling(pendulum2.body,{x:428.5, y:100});
+	sling3 = new sling(pendulum3.body,{x:456, y:100});
+	sling4 = new sling(pendulum4.body,{x:483.5, y:100});
+	sling5 = new sling(pendulum5.body,{x:511, y:100});
 	Engine.run(engine);
 }
 
@@ -40,6 +40,5 @@ function draw(){
 }
 
 function mouseDragged(){
-	console.log("no problem in function reaching");
 	Body.setPosition(pendulum1.body,{x:mouseX,y:mouseY});
 }
